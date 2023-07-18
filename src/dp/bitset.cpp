c1f72ade5573819852b7a6f615601c54b4fe9b7c
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2023-07-18 23:32:04 +0900

    Update bitset.cpp

--- src/dp/bitset.cpp
+++ src/dp/bitset.cpp
@@ -1,39 +1,42 @@
 #define private public
 #include <bitset>
 #undef private
-#include <bits/stdc++.h>
 #include <x86intrin.h>
-using namespace std;
-
-template<size_t _Nw> void _M_do_sub(_Base_bitset<_Nw> &A, const _Base_bitset<_Nw> &B) {
-	for(int i=0, c=0; i<_Nw; i++)
-		c=_subborrow_u64(c, A._M_w[i], B._M_w[i], (unsigned long long*)&A._M_w[i]);
+template <size_t _Nw>
+void _M_do_sub(_Base_bitset<_Nw> &A, const _Base_bitset<_Nw> &B) {
+  for (int i = 0, c = 0; i < _Nw; i++)
+    c = _subborrow_u64(c, A._M_w[i], B._M_w[i], (unsigned long long *)&A._M_w[i]);
 }
-template<> void _M_do_sub(_Base_bitset<1> &A, const _Base_bitset<1> &B) {
-	A._M_w-=B._M_w;
+template <>
+void _M_do_sub(_Base_bitset<1> &A, const _Base_bitset<1> &B) {
+  A._M_w -= B._M_w;
 }
-template<size_t _Nb> bitset<_Nb>& operator-=(bitset<_Nb> &A, const bitset<_Nb> &B) {
-	_M_do_sub(A, B);
-	return A;
+template <size_t _Nb>
+bitset<_Nb> &operator-=(bitset<_Nb> &A, const bitset<_Nb> &B) {
+  _M_do_sub(A, B);
+  return A;
 }
-template<size_t _Nb> inline bitset<_Nb> operator-(const bitset<_Nb> &A, const bitset<_Nb> &B) {
-	bitset<_Nb> C(A);
-	return C-=B;
+template <size_t _Nb>
+inline bitset<_Nb> operator-(const bitset<_Nb> &A, const bitset<_Nb> &B) {
+  bitset<_Nb> C(A);
+  return C -= B;
 }
-
-
-template<size_t _Nw> void _M_do_add(_Base_bitset<_Nw> &A, const _Base_bitset<_Nw> &B) {
-	for(int i=0, c=0; i<_Nw; i++)
-		c=_addcarry_u64(c, A._M_w[i], B._M_w[i], (unsigned long long*)&A._M_w[i]);
+template <size_t _Nw>
+void _M_do_add(_Base_bitset<_Nw> &A, const _Base_bitset<_Nw> &B) {
+  for (int i = 0, c = 0; i < _Nw; i++)
+    c = _addcarry_u64(c, A._M_w[i], B._M_w[i], (unsigned long long *)&A._M_w[i]);
 }
-template<> void _M_do_add(_Base_bitset<1> &A, const _Base_bitset<1> &B) {
-	A._M_w+=B._M_w;
+template <>
+void _M_do_add(_Base_bitset<1> &A, const _Base_bitset<1> &B) {
+  A._M_w += B._M_w;
 }
-template<size_t _Nb> bitset<_Nb>& operator+=(bitset<_Nb> &A, const bitset<_Nb> &B) {
-	_M_do_add(A, B);
-	return A;
+template <size_t _Nb>
+bitset<_Nb> &operator+=(bitset<_Nb> &A, const bitset<_Nb> &B) {
+  _M_do_add(A, B);
+  return A;
 }
-template<size_t _Nb> inline bitset<_Nb> operator+(const bitset<_Nb> &A, const bitset<_Nb> &B) {
-	bitset<_Nb> C(A);
-	return C+=B;
+template <size_t _Nb>
+inline bitset<_Nb> operator+(const bitset<_Nb> &A, const bitset<_Nb> &B) {
+  bitset<_Nb> C(A);
+  return C += B;
 }
